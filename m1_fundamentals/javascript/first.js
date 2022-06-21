f9564d1446a561194b64c73403d179e1eb1ff476
DengPanchol <dengpanchol10@gmail.com>
2022-06-21 12:50:38 -0400

    Learned-variables-if-state
    We learned about variables and how to use if statements

---
+++ m1_fundamentals/javascript/first.js
@@ -0,0 +1,31 @@
+// alert("Hi");
+// let num1, num2;
+// num1=10;
+// num2=20;
+// let subt = num2-num1;
+// let dvd = num2/num1;
+// let sum = num1+num2;
+// console.log("Sum is: " + sum);
+// console.log("subtraction: " + subt);
+// console.log("division is: " + dvd);
+
+//Assignment 1
+let name="Deng";
+console.log("My name is: " + name);
+alert("My name is: " + name);
+document.write("<h1>My name is Deng</h1>");
+
+//Assignment 2
+let weightInKgs = 84;
+let poundPerKg = 2.2;
+let weightInpounds = weightInKgs*poundPerKg;
+console.log("Weight in pounds " + weightInpounds+" lbs");
+
+//Assignment 3
+let height = 190
+let cmPerFt = 0.0328
+heightInFt = height*cmPerFt;
+console.log("Height in feet is: " + heightInFt+" Ft");
+
+
+
---
+++ m1_fundamentals/javascript/swap.js
@@ -0,0 +1,60 @@
+// /*
+// .1
+// //Copy this code vvv and paste above where you answer the question//
+// */
+// let inputA = 5
+// let inputB = 2
+
+// //Copy this code ^^^ and paste above where you answer the question//
+// /*
+// If inputA is greater than inputB console.log "inputA is greater than inputB"
+// If inputA is less than inputB console.log "inputA is less than inputB"
+// */
+// if(inputA>inputB){
+//     console.log("inputA is greater than inputB");
+// }else{
+//     console.log ("inputA is less than inputB");
+// }
+
+// /*
+// .2
+
+// //Copy this code vvv and paste above where you answer the question//
+// */
+// let studentMark = Math.floor(Math.random()*100)
+// let passingGrade = 70
+
+// //Copy this code ^^^ and paste above where you answer the question//
+
+// /*
+// Write an if statement after the above code to find if studentMark is greater or less than the passing grade
+// If the student does not pass console.log studentMark and then console.log "The student failed"
+// Otherwise if the student meets or exceeds passingGrade console.log studentMark and then console.log "The student passed"
+// */
+// if(studentMark<passingGrade){
+//     console.log ("The student failed")
+// }else{
+//     console.log ("The student passed")
+// }
+
+
+// /*
+// 3.
+// //Copy this code vvv and paste above where you answer the question//
+// */
+// let rng1 = Math.floor(Math.random()*10)
+// let rng2 = Math.floor(Math.random()*10)
+// let result;
+// result=rng1-rng2;
+// //Copy this code ^^^ and paste above where you answer the question//
+// /*
+// Write an if statement where result changes to either "rng1 is greater than rng2", "rng2 is greater than rng1", or "rng1 and rng2 are the same"
+// Then console.log result AFTER the last if statement
+// */
+// if(result>0){
+//     console.log("rng1 is greater than rng2");
+// }else if(result<0){
+//     console.log("rng2 is greater than rng1");
+// }else{
+//     console.log("rng1 and rng2 are the same");
+// }
