b651358660e3c61733635ed1eca8cc56d2501000
DengPanchol <dengpanchol10@gmail.com>
2022-06-28 12:44:49 -0400

    Create Arrow function and Array
    Did assignment on arrow functions and arrays.

---
+++ m1_fundamentals/javascript/arrays.js
@@ -0,0 +1,12 @@
+/*Arrays
+Question 1: Make an array with 3 different data types (string, number, boolean)
+Question 2: Create an array with 3 names then console.log the array length
+Question 3: In the previous question, console.log just the first name
+*/
+
+const array_name = ["banana","orange" ,"mango", 20, 7, 10,true, true, false,true];    
+console.log("The 7th element of the array is ", array_name[6]);
+
+const myLength = ["Deng", "Steven", "John"];
+console.log("The length of the array is ", myLength.length);
+console.log("The first element of the array is",myLength[0]);
